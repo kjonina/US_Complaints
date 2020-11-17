776a0f27f17de0da9c29610e1f441ff5ecd9972f
kjonina <kjonina@gmail.com>
2020-11-17 13:49:08 +0000

    graphs, updates to code and writing EDA results

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Netflix
+# US_Complaints
 Each week the CFPB sends thousands of consumers’ complaints about financial products and services to companies for response. Those complaints are published here after the company responds or after 15 days, whichever comes first. By adding their voice, consumers help improve the financial marketplace. The dataset was collected from [Kaggle](https://www.kaggle.com/cfpb/us-consumer-finance-complaints).
 
 
@@ -8,10 +8,8 @@ I have a lot of holes in my knowledge so I decided to outline some learning outc
 
 The purpose of this analysis for myself is to: 
 - [ ] deal with parts of code I struggle with: loc, iloc splitting text data, creating linegraph and horizontal barcharts, barcharts by a category. 
-- [ ] run analysis on ['narrative']
-- [ ] create a predictive model for complaints 
-- [ ] continue fancy-schmancy code to update README.md such as links to external url and links, etc. 
-
+- [ ] run text analysis on ['narrative']
+- [ ] create a predictive model for complaints
 
 ### Variables
 
@@ -21,4 +19,78 @@ There are 18 variables and 556 000 observations.
 
 New Variables:
 ['days_to_process']  compares the number of days between ['date_sent_to_company'] and ['date_received']
+
+## Exploring EDA
+
+### What states have the most number of complaints?
+
+![state](https://github.com/kjonina/US_Complaints/blob/main/Graphs/state.png)
+
+### What products have the most number of complaints?
+
+![products](https://github.com/kjonina/US_Complaints/blob/main/Graphs/Products.png)
+
+### What products have the most number of complaints?
+
+![products](https://github.com/kjonina/US_Complaints/blob/main/Graphs/Products.png)
+
+### What was the most popular method of submission??
+
+![submission](https://github.com/kjonina/US_Complaints/blob/main/Graphs/submission.png)
+
+
+There is a big discrepancy between methods of submission. How was fax more popular than email? 
+
+| Submission Method | Count |
+| ---------------- | ------------- |
+| Web | 361338 |
+| Referral | 109379 |
+| Phone | 40026 |
+| Postal mail | 36752 |
+| Fax| 8118 |
+| Email| 344 |
+
+
+### What was the most common response from the companies?
+
+![response](https://github.com/kjonina/US_Complaints/blob/main/Graphs/response.png)
+
+| Submission Method | Count |
+| ---------------- | ------------- |
+| Closed with explanation | 404293 |
+| Closed with non-monetary relief | 70237 |
+| Closed with monetary relief | 38262 |
+| Closed without relief | 17909 |
+| Closed | 13399 |
+| Closed with relief | 5305 |
+| In progress | 3763 |
+| Untimely response | 2789 |
+
+
+### Was that the companies response any good?
+
+![timely](https://github.com/kjonina/US_Complaints/blob/main/Graphs/timely.png)
+
+Only 2.5% of compaints did not receive a timely response.
+
+| Was there a timely response? | Count |
+| ---------------- | ------------- |
+| Yes | 541909 |
+| No | 14048 |
+
+
+### What is the length of time between the date sent to the company and date received by product?
+
+![response_by_product](https://github.com/kjonina/US_Complaints/blob/main/Graphs/response_by_product.png)
+
+ 
+ # TO DO -> 
+ 
+ - draw a time graph for date_received + date_sent_to_company!
+ - draw a graph the company by product type
+ - draw a graph for days_process
+ - find the  most common words in Narrative. 
+
+ - creating a wordcloud for issue 
+ - find methods to analyse text.
 
--- US_Complaints.py
+++ US_Complaints.py
@@ -21,114 +21,96 @@ from sklearn.feature_extraction.text import CountVectorizer #Bag of Words
 
 
 
-# The dataset used was collected from the following website:
+# The df used was collected from the following website:
 # https://www.kaggle.com/cfpb/us-consumer-finance-complaints
 
 # read the CSV file
-dataset = pd.read_csv('consumer_complaints.csv')
+df = pd.read_csv('consumer_complaints.csv')
 
 # Will ensure that all columns are displayed
 pd.set_option('display.max_columns', None) 
 
 # prints out the top 5 values for the datasef
-print(dataset.head())
+print(df.head())
 
-# checking the dataset shape
-print(dataset.shape)
-# (555957, 18)
+# checking shape
+print("The df has {} rows and {} columns.".format(*df.shape))
+
+# ... and duplicates
+print("It contains {} duplicates.".format(df.duplicated().sum()))
 
 
 # prints out names of columns
-print(dataset.columns)
+print(df.columns)
 
 # This tells us which variables are object, int64 and float 64. This would mean that 
 # some of the object variables might have to be changed into a categorical variables and int64 to float64 
 # depending on our analysis.
-print(dataset.info())
+print(df.info())
 
 
 # checking for missing data
-dataset.isnull().sum() 
+df.isnull().sum() 
 
 # making object into categorical variables
-dataset['product'] = dataset['product'].astype('category')
-dataset['sub_product'] = dataset['sub_product'].astype('category')
-dataset['company'] = dataset['company'].astype('category')
-dataset['state'] = dataset['state'].astype('category')
-dataset['submitted_via'] = dataset['submitted_via'].astype('category')
-dataset['company_response_to_consumer'] = dataset['company_response_to_consumer'].astype('category')
-dataset['timely_response'] = dataset['timely_response'].astype('category')
-dataset['consumer_disputed?'] = dataset['consumer_disputed?'].astype('category')
+df['product'] = df['product'].astype('category')
+df['sub_product'] = df['sub_product'].astype('category')
+df['company'] = df['company'].astype('category')
+df['state'] = df['state'].astype('category')
+df['submitted_via'] = df['submitted_via'].astype('category')
+df['company_response_to_consumer'] = df['company_response_to_consumer'].astype('category')
+df['timely_response'] = df['timely_response'].astype('category')
+df['consumer_disputed?'] = df['consumer_disputed?'].astype('category')
 
 # checking data to check that all objects have been changed to categorical variables.
-dataset.info()
+df.info()
 
 # =============================================================================
 # Converting 'date_received' and 'date_sent_to_company' to datetime
 # =============================================================================
 
-dataset['date_received'] = dataset['date_received'].astype('datetime64')
-dataset['date_sent_to_company'] = dataset['date_sent_to_company'].astype('datetime64')
-
-
-
-print(dataset.info())
+df['date_received'] = df['date_received'].astype('datetime64')
+df['date_sent_to_company'] = df['date_sent_to_company'].astype('datetime64')
 
 #creating column to asses the number of days to process claim
-dataset['days_to_process'] = dataset['date_sent_to_company'] - dataset['date_received']
+df['days_to_process'] = df['date_sent_to_company'] - df['date_received']
 
-plt.figure(figsize = (12, 8))
-sns.countplot(x = 'days_to_process', data = dataset, palette = 'viridis', order = dataset['days_to_process'].value_counts().index)
-plt.xticks(rotation = 90)
-plt.title('Days Needed for the Company to Respond', fontsize = 16)
-plt.ylabel('count', fontsize = 14)
-plt.xlabel('Number of days to Process', fontsize = 14)
-plt.show()
+print(df['days_to_process'])
 
-print(dataset['days_to_process'])
 
 # =============================================================================
 # Examining States
 # =============================================================================
-dataset['state'].unique()
 
-dataset.groupby(['state']).size().sort_values(ascending=False)
+df['state'].unique()
+
+# Examines the States that have complaints
+df.groupby(['state']).size().sort_values(ascending=False)
 
 # Examines the top 25 States that have complaints
+sns.set()
 plt.figure(figsize = (12, 8))
-sns.countplot(x = 'state', data = dataset, palette = 'viridis', order = dataset['state'].value_counts().head(25).index)
+sns.countplot(x = 'state', data = df, palette = 'magma', order = df['state'].value_counts().head(25).index)
 plt.xticks(rotation = 90)
-plt.title('Breakdown of TOP 25 States', fontsize = 16)
-plt.ylabel('count', fontsize = 14)
+plt.title('Breakdown of Top 25 States', fontsize = 16)
+plt.ylabel('Number of Complaints', fontsize = 14)
 plt.xlabel('States', fontsize = 14)
 plt.show()
 
 # =============================================================================
 # Examining Products
 # =============================================================================
-dataset['product'].unique()
+df['product'].unique()
 
-dataset.groupby(['product']).size().sort_values(ascending=False)
-#Mortgage                   186475
-#Debt collection            101052
-#Credit reporting            91854
-#Credit card                 66468
-#Bank account or service     62563
-#Consumer Loan               20990
-#Student loan                15839
-#Payday loan                  3877
-#Money transfers              3812
-#Prepaid card                 2470
-#Other financial service       557
-#dtype: int64
+# Examines the products
+df.groupby(['product']).size().sort_values(ascending=False)
 
-# Examines the top 25 countries that make a reservation
+# Examines the products
 plt.figure(figsize = (12, 8))
-sns.countplot(x = 'product', data = dataset, palette = 'viridis', order = dataset['product'].value_counts().index)
-plt.xticks(rotation = 90)
+sns.countplot(y = 'product', data = df, palette = 'viridis', order = df['product'].value_counts().index)
 plt.title('Breakdown of Products', fontsize = 16)
-plt.ylabel('count', fontsize = 14)
-plt.xlabel('Products', fontsize = 14)
+plt.ylabel('Type of Products', fontsize = 14)
+plt.xlabel('Number of Complaints', fontsize = 14)
 plt.show()
 
 
@@ -136,163 +118,138 @@ plt.show()
 # =============================================================================
 # Examining Companies
 # =============================================================================
-dataset['company'].unique()
+df['company'].unique()
 
-dataset.groupby(['company']).size().sort_values(ascending=False)
-#No     443823
-#Yes    112134
+df.groupby(['company']).size().sort_values(ascending=False)
 
 # Examines the top 25 companies which had the most complaints
 plt.figure(figsize = (12, 8))
-sns.countplot(x = 'company', data = dataset, palette = 'viridis', order = dataset['company'].value_counts().head(25).index)
-plt.xticks(rotation = 90)
+sns.countplot(y = 'company', data = df, palette = 'magma', order = df['company'].value_counts().head(25).index)
 plt.title('Breakdown of Companies Involved', fontsize = 16)
-plt.ylabel('count', fontsize = 14)
-plt.xlabel('Companies', fontsize = 14)
-plt.show()
-
-
-# creates a pie chart 
-fig = plt.figure(figsize = (20,10))
-labels = dataset['company'].value_counts().head(25).index.tolist()
-sizes = dataset['company'].value_counts().head(25).tolist()
-plt.pie(sizes, labels = labels, autopct = '%1.1f%%',
-        shadow = False, startangle = 30)
-plt.title('Breakdown of Top 25 Companies', fontdict = None, position = [0.48,1], size = 'xx-large')
+plt.ylabel('Companies', fontsize = 14)
+plt.xlabel('Number of Complaints', fontsize = 14)
 plt.show()
 
 
 # =============================================================================
 # Examining Methods of Submission
 # =============================================================================
-dataset['submitted_via'].unique()
+df['submitted_via'].unique()
 
-dataset.groupby(['submitted_via']).size().sort_values(ascending=False)
-#Web            361338
-#Referral       109379
-#Phone           40026
-#Postal mail     36752
-#Fax              8118
-#Email             344
+# Examines method of submission
+df.groupby(['submitted_via']).size().sort_values(ascending=False)
 
 # Examines method of submission
 plt.figure(figsize = (12, 8))
-sns.countplot(x = 'submitted_via', data = dataset, palette = 'viridis', order = dataset['submitted_via'].value_counts().head(25).index)
-plt.xticks(rotation = 90)
+sns.countplot(y = 'submitted_via', data = df, palette = 'viridis', order = df['submitted_via'].value_counts().head(25).index)
 plt.title('Breakdown of Methods of Submission', fontsize = 16)
-plt.ylabel('count', fontsize = 14)
-plt.xlabel('Methods of Submission', fontsize = 14)
+plt.ylabel('Methods of Submission', fontsize = 14)
+plt.xlabel('Number of Complaints', fontsize = 14)
 plt.show()
-
-
-# creates a pie chart 
-fig = plt.figure(figsize = (20,10))
-labels = dataset['submitted_via'].value_counts().head(25).index.tolist()
-sizes = dataset['submitted_via'].value_counts().head(25).tolist()
-plt.pie(sizes, labels = labels, autopct = '%1.1f%%',
-        shadow = False, startangle = 30)
-plt.title('Breakdown of Submission Methods', fontdict = None, position = [0.48,1], size = 'xx-large')
-plt.show()
-
 
 # =============================================================================
 # Examining Company Response to Consumer
 # =============================================================================
-dataset['company_response_to_consumer'].unique()
+df['company_response_to_consumer'].unique()
 
-dataset.groupby(['company_response_to_consumer']).size().sort_values(ascending=False)
-#Closed with explanation            404293
-#Closed with non-monetary relief     70237
-#Closed with monetary relief         38262
-#Closed without relief               17909
-#Closed                              13399
-#Closed with relief                   5305
-#In progress                          3763
-#Untimely response                    2789
+
+# Examines company responses to the company
+df.groupby(['company_response_to_consumer']).size().sort_values(ascending=False)
 
 # Examines company responses to the company
 plt.figure(figsize = (12, 8))
-sns.countplot(x = 'company_response_to_consumer', data = dataset, palette = 'viridis', order = dataset['company_response_to_consumer'].value_counts().index)
-plt.xticks(rotation = 90)
+sns.countplot(y = 'company_response_to_consumer', data = df, palette = 'viridis', order = df['company_response_to_consumer'].value_counts().index)
 plt.title('Breakdown of Company Response to Consumer', fontsize = 16)
-plt.ylabel('count', fontsize = 14)
-plt.xlabel('Company Response', fontsize = 14)
+plt.ylabel('Company Response', fontsize = 14)
+plt.xlabel('count', fontsize = 14)
 plt.show()
 
 
-# creates a pie chart 
+# =============================================================================
+# Examining days_to_process
+# =============================================================================
+
+time_df = df['days_to_process'].value_counts().head(20)
+
 fig = plt.figure(figsize = (20,10))
-labels = dataset['company_response_to_consumer'].value_counts().index.tolist()
-sizes = dataset['company_response_to_consumer'].value_counts().tolist()
-plt.pie(sizes, labels = labels, autopct = '%1.1f%%',
-        shadow = False, startangle = 30)
-plt.title('Breakdown of Breakdown of Company Response to Consumer', fontdict = None, position = [0.48,1], size = 'xx-large')
+sns.lineplot(data = time_df)
+plt.title('The Response in Days between ', fontsize = 20)
+plt.ylabel('count', fontsize = 14)
+plt.xlabel('Duration in Days', fontsize = 14)
 plt.show()
+
+
+# =============================================================================
+# Examining days_to_process by Product
+# ============================================================================= 
+product_days = df.groupby(['product']
+        )['days_to_process'].sum().sort_values(ascending = False)
+
+
+#Creating a graph for TV Shows
+plt.figure(figsize = (12, 8))
+sns.barplot(y = "product", x = "days_to_process", data = df,
+                 palette = 'Reds_d')
+plt.set_title('Top 15 TV Shows Mid-Covid', fontsize = 20)
+plt.set_ylabel('TV Shows', fontsize = 14)
+plt.set_xlabel('Duration', fontsize = 14)
+plt.set_xticklabels(plt.get_xticklabels(), rotation = 90)
+
 
 # =============================================================================
 # Examining Reponses
 # =============================================================================
-dataset['timely_response'].unique()
+df['timely_response'].unique()
 
-dataset.groupby(['timely_response']).size().sort_values(ascending=False)
-#Yes    541909
-#No      14048
+## Examines the response
+df.groupby(['timely_response']).size().sort_values(ascending=False)
 
-# Examines the response
-plt.figure(figsize = (12, 8))
-sns.countplot(x = 'timely_response', data = dataset, palette = 'viridis', order = dataset['timely_response'].value_counts().index)
-plt.xticks(rotation = 90)
-plt.title('Breakdown of Responses', fontsize = 16)
-plt.ylabel('count', fontsize = 14)
-plt.xlabel('Timely Response', fontsize = 14)
-plt.show()
+## Examines the response
+#plt.figure(figsize = (12, 8))
+#sns.countplot(x = 'timely_response', data = df, palette = 'viridis', order = df['timely_response'].value_counts().index)
+#plt.title('Breakdown of Responses', fontsize = 16)
+#plt.ylabel('count', fontsize = 14)
+#plt.xlabel('Timely Response', fontsize = 14)
+#plt.show()
 
 # creates a pie chart 
 fig = plt.figure(figsize = (20,10))
-labels = dataset['timely_response'].value_counts().index.tolist()
-sizes = dataset['timely_response'].value_counts().tolist()
+labels = df['timely_response'].value_counts().index.tolist()
+sizes = df['timely_response'].value_counts().tolist()
 plt.pie(sizes, labels = labels, autopct = '%1.1f%%',
         shadow = False, startangle = 30)
 plt.title('Was there a timely response to the complaint?', fontdict = None, position = [0.48,1], size = 'xx-large')
 plt.show()
 
 # =============================================================================
-# 
+# Exploring Timely Response by Product
 # =============================================================================
-# Table I want to create:
-#                       Yes                 NO
-# Mortgage
-# Student Debt
 
-# Stacked barchart!
-
-dataset.groupby('product')['timely_response'].value_counts()
-
-fig = plt.figure(figsize = (20,10))
-labels = dataset['product'].value_counts().index.tolist()
-sizes = dataset['product'].value_counts().tolist()
-plt.pie(sizes, labels = labels, autopct = '%1.1f%%',
-        shadow = False, startangle =30)
-plt.title('Matching the Reserved Room and Assigned Room', fontdict = None, position= [0.48,1], size = 'xx-large')
-plt.show()
+df.groupby('product')['timely_response'].value_counts()
 
 
+# isolating variables in a table
+product_timely = pd.DataFrame({'product': df['product'],
+                   'timely_response': df['timely_response']})
+
+#pivoting the table
+product_timely_pivot = pd.pivot_table(
+        product_timely, index = ['timely_response'], columns = ['product'], aggfunc=len)
+
+# print pitot table to insure it works
+print(product_timely_pivot)
+
+# creating a graph
+plt.figure(figsize = (12, 8))
 
 
-
-
+sns.catplot(x = 'product', hue = 'timely_response', kind = 'count', palette = 'pastel', edgecolor = '.6', data = product_timely_pivot)
 
 
 
-
-# =============================================================================
-# Splitting the data into timely or late responses
-# =============================================================================
-# splitting data on 'Yes' or 'No' Timely Responses
-# retrieving everyone who has NOT been timely responded
-timely_response_data = dataset[dataset['timely_response'].isin(['Yes'])]
-
-late_response_data  = dataset[dataset['timely_response'].isin(['No'])]
+plt.title('Breakdown of Ratings by Content Type', fontsize = 16)
+plt.ylabel('Rating', fontsize = 14)
+plt.xlabel('count', fontsize = 14)
 
 
 
@@ -302,7 +259,7 @@ late_response_data  = dataset[dataset['timely_response'].isin(['No'])]
 # =============================================================================
 
 # Only interested in data with consumer complaints
-d = dataset[dataset['issue'].notnull()]
+d = df[df['issue'].notnull()]
 
 
 sns.set(style="white", color_codes=True)
@@ -356,11 +313,11 @@ plt.show()
 # =============================================================================
 # TO DO -> 
 # =============================================================================
-# draw a time graph for date_received!
+# draw a time graph for date_received + date_sent_to_company!
 # draw a graph the company by product type
 # draw a graph for days_process
 # find the  most common words in Narrative. 
-
+#
 # creating a wordcloud for issue was not informative 
 # find better methods to analyse text.
 
@@ -370,7 +327,7 @@ plt.show()
 # =============================================================================
 
 # Only interested in data with consumer complaint narrative
-narrative_dataset = dataset[dataset['consumer_complaint_narrative'].notnull()]
+narrative_df = df[df['consumer_complaint_narrative'].notnull()]
 
 
 """
@@ -380,7 +337,7 @@ Remove XXXX from the list!
 import re
 
 review_list=[]
-for review in narrative_dataset.consumer_complaint_narrative:
+for review in narrative_df.consumer_complaint_narrative:
     review = re.sub("[^a-zA-z]"," ",review) # if expression in the sentence is not a word then this code change them to space
     review = review.lower() # turns all word in the sentence into lowercase.
     review = nltk.word_tokenize(review) # splits the words that are in the sentence from each other.
